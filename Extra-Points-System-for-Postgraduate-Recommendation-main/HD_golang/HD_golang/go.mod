c9726214a1bfc52e5a1e6f921718c0a9e9cf3b73
Abmiron <3181630533@qq.com>
2025-11-14 15:40:03 +0800

    eleventh week

--- Extra-Points-System-for-Postgraduate-Recommendation-main/HD_golang/HD_golang/go.mod
+++
@@ -1,9 +0,0 @@
-module hd_golang
-
-go 1.25.1
-
-require (
-	github.com/golang-jwt/jwt/v4 v4.5.2
-	github.com/lib/pq v1.10.9
-	golang.org/x/crypto v0.43.0
-)
--- Extra-Points-System-for-Postgraduate-Recommendation-main/test-vue-app/clean-cache.js
+++
@@ -1,8 +0,0 @@
-// 清理localStorage中的数据
-try {
-  localStorage.removeItem('token');
-  localStorage.removeItem('userInfo');
-  console.log('缓存已清理');
-} catch (error) {
-  console.error('清理缓存时出错:', error);
-}
